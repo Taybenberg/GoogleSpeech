b49ecccf37d40e3b09f4b326d8334b34ca4620fb
Вдовенко Віталій <taybenberg@gmail.com>
2019-10-11 13:41:52 +0300

    GoogleSTT

---
+++ GoogleSTT/gSTT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Google.Cloud.Speech.V1;
+
+namespace GoogleSTT
+{
+    public class gSTT
+    {
+        public string Result { get; private set; }
+
+        public gSTT(string speechURI, int sampleRateHertz, string languageCode = "uk-UA", RecognitionConfig.Types.AudioEncoding audioEncoding = RecognitionConfig.Types.AudioEncoding.OggOpus)
+        {
+            var speech = SpeechClient.Create();
+
+            var response = speech.Recognize(new RecognitionConfig()
+            {
+                Encoding = audioEncoding,
+                SampleRateHertz = sampleRateHertz,
+                LanguageCode = languageCode,
+                EnableAutomaticPunctuation = true,
+            }, RecognitionAudio.FetchFromUri(speechURI));
+
+            Result = string.Empty;
+
+            foreach (var result in response.Results)
+                foreach (var alternative in result.Alternatives)
+                    Result += alternative.Transcript;
+
+            if (string.IsNullOrEmpty(Result))
+                throw new ArgumentNullException();
+        }
+    }
+}
